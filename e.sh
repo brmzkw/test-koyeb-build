a12a837ac7b13d205117f1de8c754f3f46aef1e7
Julien Castets <castets.j@gmail.com>
2023-05-25 10:37:55 +0200

    display command

--- e.sh
+++ e.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-echo welcome to the custom entrypointtttttttttttttttt
+echo welcome to the custom entrypointtttttttttttttttt, argumnets provided: $@
 
 exec /docker-entrypoint.sh nginx -g 'daemon off;'
