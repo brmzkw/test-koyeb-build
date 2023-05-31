ef2b80dd393097773776af1beb0cbe2ec2f6cdb7
Julien Castets <castets.j@gmail.com>
2023-05-31 13:50:23 +0200

    Dump env

--- e.sh
+++ e.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 
-echo welcome to the custom entrypointtttttttttttttttt, argumnets provided: $@
+echo =========================
+echo Custom entrypoint called with: $@
+echo Environment:
+env
+echo =========================
 
 exec /docker-entrypoint.sh nginx -g 'daemon off;'
