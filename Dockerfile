33106e6570ac9795544cd4990e2f947529e9a177
Julien Castets <castets.j@gmail.com>
2023-05-31 19:02:47 +0200

    Buildargs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM nginx
 
+ARG BUILDARG1
+ARG BUILDARG2
+ARG BUILDARG3
+RUN env >> /tmp/buildenv
+
 COPY koyeb.html /usr/share/nginx/html/index.html
 
 COPY e.sh /e.sh
--- e.sh
+++ e.sh
@@ -3,8 +3,14 @@
 
 echo =========================
 echo Custom entrypoint called with: $@
-echo Environment:
+echo =========================
+echo =      Environment      =
+echo =========================
 env
+echo =========================
+echo =       Buildenv        =
+echo =========================
+cat /tmp/buildenv
 echo =========================
 
 exec /docker-entrypoint.sh nginx -g 'daemon off;'
